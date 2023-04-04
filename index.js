7d480ef0bddde5b5666a17896bebd8ca7d4ae085
Doyle Turner <3182114+ExpressDead@users.noreply.github.com>
2023-04-03 20:32:26 -0500

    Cleanup: Remove basic sprite animiation test. Sprite works

--- index.js
+++ index.js
@@ -6,21 +6,4 @@ window.addEventListener('load', e => {
 
     ctx = canvas.getContext('2d');
 
-    let sw = 900;
-    let sh = 600;
-    frameX = 0;
-
-    let image = document.getElementById('robot');
-
-
-    function animate(){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.drawImage(image, frameX * sw, 0, 909, 600, (canvas.width * 0.5) - 100, (canvas.height * 0.5) + 150, 175, 100);
-
-        frameX < 20 ? frameX++ : frameX = 0;
-        requestAnimationFrame(animate);
-    }
-
-    animate();
-
 });
